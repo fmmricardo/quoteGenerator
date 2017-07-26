f98529492176b9ead517aaa10821f4edaac9665f
fmmricardo <ricardofilipe5@sapo.pt>
2017-07-26 02:11:32 +0200

    add files to repo

---
+++ js/quotesList.js
@@ -0,0 +1,67 @@
+//array with the quotes
+var quoteList = [
+  {
+    quote:'Ignorance is a poison and knowledge will nourish',
+    source:'KRS-One',
+    citation:'“Word From Our Sponsor,” from Boogie Down Productions’ Criminal Minded',
+    date:'1987',
+    tag: 'music'
+
+  },
+  {
+    quote:'Whatever you goin through, could always be much worse,Don’t make a mistake, mistakin’ your blessings for a curse.',
+    source:'Joe Budden',
+    citation:'Welcome to Real Life,” Mood Muzik 4: A Turn for the Worst',
+    date:'2010',
+    tag: 'music',
+  },
+  {
+    quote:'The sky is falling, the wind is calling,Stand for something, or die in the morning.',
+    source:'Kendrick Lamar',
+    citation:'HiiPower,” Section.80',
+    date:'2011',
+    tag: 'music'
+  },
+  {
+    quote:'In this life ain’t no happy endings Only pure beginnings followed by years of sinning and fake repentance.',
+    source:'J. Cole',
+    citation:'Runaway,” Born Sinner',
+    date:'2013',
+    tag: 'music'
+  },
+  {
+    quote:'I’m only 19 but my mind is older When things get for real, my warm heart turns cold.',
+    source:'Prodigy',
+    citation:'Shook Ones Part II,” from Mobb Deep’s The Infamous',
+    date:'1987',
+    tag: 'music'
+  },
+  {
+    quote:'“Obstacles don’t have to stop you. If you run into a wall, don’t turn around and give up. Figure out how to climb it, go through it, or work around it.” ',
+    source:'Michael Jordan',
+    citation:'Shook Ones Part II,” from Mobb Deep’s The Infamous',
+    date:'1984-2003',
+    tag: 'sports'
+  },
+  {
+    quote:'In baseball and in business, there are three types of people. Those who make it happen, those who watch it happen, and those who wonder what happened.',
+    source:'Tommy Lasorda',
+    citation:' Hall of Fame baseball player and manager',
+    date:'1954-1956, player; 1976-1996',
+    tag: 'music'
+  },
+  {
+    quote:'“Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.”',
+    source:'Norman Vincent Peale',
+    citation:' ',
+    date:'',
+    tag: 'motivational'
+  },
+  {
+    quote:'“Aim for the moon. If you miss, you may hit a star.”',
+    source:'W. Clement Stone',
+    citation:' ',
+    date:'',
+    tag: 'motivational'
+  }
+]
---
+++ js/script.js
@@ -0,0 +1,96 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener("click", IntervalOn , false);
+
+//initialiye riables
+ var outputdiv = "";
+ var getrandomQuote="";
+ var quoteList = [];
+ var intervalID = setInterval(printQuote,30);
+
+
+
+// function print message
+
+ function printMessage (message){
+
+  var outputdiv = document.getElementById('quote-box');
+      outputdiv.innerHTML = message;
+      console.log(message);
+}
+
+//generate a random quote
+
+  var getrandomQuote = function() {
+    randomQuote = quoteList[Math.floor(Math.random() * quoteList.length)];
+  return randomQuote;
+  }
+
+
+  //generate the random background
+
+  function generateRBG() {
+
+    var r = Math.floor(Math.random()* 100);
+    var b = Math.floor(Math.random()* 150);
+    var g = Math.floor(Math.random()* 250);
+
+    var RGB = "rgb(" +r + "," +b+ "," +g +")";
+    return (RGB)
+    }
+
+  // Change the background color
+
+   function bgColor(){
+
+    var bg = generateRBG();
+    document.body.style.background = bg;
+  }
+
+
+// use the ramdom quote genetated and add it into the HTML
+  //function printQuote
+
+
+
+  function printQuote (){
+
+      //get reandom quote
+      getRamndomeQuotes = getrandomQuote()
+      //return quote
+      message= '<p class="quote">' + getRamndomeQuotes.quote + '</p>'
+      //return the source
+      message+= '<p class="source">' + getRamndomeQuotes.source + '</p>'
+      //reuurn the citation
+      if (getRamndomeQuotes.citation){
+      message+= '<span class="citation">' + getRamndomeQuotes.citation +  '</span>'
+      }
+
+      if(getRamndomeQuotes.date){
+        // return the date
+        message+= '<span class="year">' + getRamndomeQuotes.date + '</span>'
+      }
+      if (getRamndomeQuotes.citation){
+      message+= '<span class="citation">' + getRamndomeQuotes.citation +  '</span>'
+      }
+      if (getRamndomeQuotes.tag){
+      message+= '<p class="citation">' + getRamndomeQuotes.tag +  '</p>'
+      }
+
+        printMessage (message);
+        bgColor();
+  };
+
+
+  function IntervalOn(){
+
+    IntervalOff();
+    intervalID = setInterval(printQuote,30);
+
+  }
+  //quoteInterval = setInterval(printQuote,30);
+  function IntervalOff(){
+    clearInterval(intervalID);
+
+  }
